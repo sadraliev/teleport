fba2322c8858a406d69728b2d4b8dce99b4e947b
sadraliev <a.sadraliev@gmail.com>
2025-04-05 13:11:17 -0500

    Initial Commit

---
+++ teleport/teleportApp.swift
@@ -0,0 +1,17 @@
+//
+//  teleportApp.swift
+//  teleport
+//
+//  Created by Aibek Sadraliev on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct teleportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ teleportTests/teleportTests.swift
@@ -0,0 +1,16 @@
+//
+//  teleportTests.swift
+//  teleportTests
+//
+//  Created by Aibek Sadraliev on 4/5/25.
+//
+
+import Testing
+
+struct teleportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
